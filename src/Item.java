b09f803b215bec79fec92e94a03d490b02b12feb
brandan <brandan@brandan-G73Jw>
2015-03-06 06:30:31 -0800

    Fixed some stuff

--- src/Item.java
+++ src/Item.java
@@ -1,53 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class Item
+{
+    private String price;
 
-/**
- *
- * @author kalebstriplin
- */
+    private String quanty;
 
+    private String name;
 
-public class Item {
-    
-    public Item() {
-        this.Quantity = 0;
-        this.Price = 0;
-        this.Name = null;
-    }
-    
-    public Item(int number, int price, String name) {
-        this.Quantity = number;
-        this.Price = price;
-        this.Name = name;
+    public String getPrice ()
+    {
+        return price;
     }
 
-    public int getQuantity() {
-        return this.Quantity;
-    }
-    public int getPrice() {
-        return this.Price;
-    }
-    public String getName() {
-        return this.Name;
-    }
-    
-    public boolean compare(String aName) {
-      if(Name.toLowerCase().contains(aName.toLowerCase())) 
-          return true;
-      else
-        return false;
-    }
-    
-    public String toString() {
-        return Quantity.toString() + " " + Price.toString() + " " + Name + "\n";
-        
+    public void setPrice (String price)
+    {
+        this.price = price;
     }
 
-    private Integer Quantity;
-    private Integer Price;
-    private String Name;
-    public enum Type {debit, credit, cash};
+    public String getQuanty ()
+    {
+        return quanty;
+    }
+
+    public void setQuanty (String quanty)
+    {
+        this.quanty = quanty;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [price = "+price+", quanty = "+quanty+", name = "+name+"]";
+    }
 }
